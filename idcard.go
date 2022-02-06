d8b359255cb0a3fcecdd2fe89f33151ce0af1d32
ukuz90 <zhuangjinjin90@gmail.com>
2022-02-06 16:05:06 +0800

    update idcard add verificate function

--- idcard.go
+++ idcard.go
@@ -1,9 +1,32 @@
 package gtools;
 
-struct IdCardTool {
+import (
+	"strconv"
+)
 
-}
+var factors = []int {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2}
+var masks = []int {1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2}
 
-func Match(idCardStr string) bool 
-{	return true;
+func Verificate(idCardStr string) bool {
+	if (len(idCardStr) != 18) {
+		return false
+	}
+	var sum int
+	for i := 0; i < 17; i++ {
+		digit, _ := strconv.Atoi(idCardStr[i:i+1])
+		sum += digit * factors[i]
+	}
+	calMaskIndex := sum % 11
+	var mask int
+	if (idCardStr[17:] == "X") {
+		mask = 10
+	} else {
+		mask, _ = strconv.Atoi(idCardStr[17:])
+	}
+
+	if mask == masks[calMaskIndex] {
+		return true
+	} else {
+		return false
+	}
 }
